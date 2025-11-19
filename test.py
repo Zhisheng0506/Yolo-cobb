436b1ad69dd6236dadf2780385917927be087355
Kmon <you@example.com>
2025-11-19 21:01:51 +0800

    备份这一块

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 import torch
 from ultralytics import YOLO
-model = YOLO('ultralytics/cfg/models/11/yolo11-obb.yaml')
-model.model.to('cpu')
-x = torch.randn(1, 3, 640, 640)
-with torch.no_grad():
-    y = model.model(x)
-print('forward outputs:', y.shape)
 
-print('forward outputs type:', type(y))
+model=YOLO("yolo11n-obb.pt")
+
+results=model.train(data="ultralytics/cfg/datasets/DOTAv1.yaml",epochs=100,batch=8,imgsz=512,workers=4,device=0)
